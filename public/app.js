1e2108cd6e3ff4842fb14d224598c04e4e28a328
mabadia <angel.beyou95@gmail.com>
2023-06-26 19:48:34 -0700

    psuedo code

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// shuffel cards 
+// keeps cards flipped 
+// flip function (check for a match)
+// for loop for points 
+// count down funtion 
+// unflip
+// pop up message for end score 
+// 
+// 
+// 
