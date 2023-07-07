03642bd0b6fde7670e2a610cd23b348903b076f4
mabadia <angel.beyou95@gmail.com>
2023-07-07 08:16:54 -0700

    start game,bgmusic,countdown function

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ This is a card matching game the goals are to match a set of 16 cards. Each matc
 
 # All photos are grabbed from Pixabay
 
+# HTML and CSS
+Reaching a goal that the cards had a nice transitions and looked with the effects was a challenge. Once that was done the next struggle was creating javascript that allowed the cards to function properly to best reach the games goals.
+# Javascript 
+First getting the audio to play was a bit of a challenge to get uploaded but with some guidance and hard work found a way to make it work onto the page and then the next challenge was the shuffle 
 
-stretch goals- Every failed attempt cost you 50 points. matches back to back are double points. refactor code and make sure that the code isnt repetetive. 
+# Strech Goals
+stretch goals-difficulty of game gets difficult. Every failed attempt cost you 50 points. matches back to back are double points. refactor code and make sure that the code isnt repetetive. 
--- public/app.js
+++ public/app.js
@@ -2,9 +2,53 @@
 // keeps cards flipped 
 // flip function (check for a match)
 // for loop for points 
-// count down funtion 
 // unflip
 // pop up message for end score 
-// audio files 
-// 
-// 
+
+
+
+
+// click to start
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+// ready function
+function ready() {
+    let startText = Array.from(document.getElementsByClassName('start-text'));
+    let cards = Array.from(document.getElementsByClassName('card'));
+
+    startText.forEach(start => {
+        start.addEventListener('click', () => {
+            start.classList.remove('visible');
+            startGame()
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            //game.flipCard(card);
+        });
+    });
+}
+// on click game starts
+function startGame() {
+    bgMusic();
+    countdown();
+}
+// background music
+function bgMusic() {
+    let music = new Audio('../assets/audio/jazz-happy-110855.mp3');
+    music.loop = true;
+    music.play();
+}
+// countdown timer
+function countdown() {
+    let timer = document.getElementById('time-remaining');
+    let timeRemaining = 100;
+    timer.innerText = timeRemaining;
+    gameInterval = setInterval(() => {
+        timeRemaining--;
+        timer.innerText = timeRemaining;
+    }, 1000);
+} 
